d855b0072bae05df226f14b914c52a0261d12604
Nguyen Phuoc Loc <nguyenphuoclocSE200425@gmail.com>
2026-06-22 10:49:56 +0700

    Add exercise 004

---
+++ 004 divided.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    double number1;
+    printf("");
+    scanf("%lf",&number1);
+
+    double divided = number1 / 3;
+    printf("%.2lf",divided);
+    return 0;
+
+}
