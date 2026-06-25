a19aa7e2782034dc582c70a2dc64a5a30fcf31ea
Nguyen Phuoc Loc <nguyenphuoclocSE200425@gmail.com>
2026-06-25 07:40:48 +0700

    Update exercice C

---
+++ 006 squareRoot.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	double number1;
+	printf("");
+	scanf("%lf",&number1);
+	
+	double squareRoot = sqrt(number1); // Compute the square root with sqrt()
+	printf("%.2lf",squareRoot); t()
+	return 0;
+	
+}
